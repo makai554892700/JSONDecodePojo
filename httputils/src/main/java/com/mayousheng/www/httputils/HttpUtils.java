cb2ac60826856e0d4aac64c16d76d02d8a32957b
makai <makai554892700@sina.com>
2019-09-11 16:58:50 +0800

    change:升级httputils

--- httputils/src/main/java/com/mayousheng/www/httputils/HttpUtils.java
+++ httputils/src/main/java/com/mayousheng/www/httputils/HttpUtils.java
@@ -5,12 +5,20 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.Proxy;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * Created by marking on 2016/11/26.
@@ -44,6 +52,88 @@ public class HttpUtils {
     public interface IWebSessionBack extends CallBack {
         void onCallback(int status, String message, Map<String, List<String>> heard
                 , String sessionId, byte[] data);
+    }
+
+    public void getURLResponse(String urlString, HashMap<String, String> heads, Proxy proxy
+            , IWebCallback iWebCallback) {
+        if (urlString != null) {
+            HttpURLConnection conn = null; //连接对象
+            InputStream is = null;
+            ByteArrayOutputStream baos = null;
+            try {
+                URL url = new URL(urlString); //URL对象
+                if (proxy == null) {
+                    if (urlString.startsWith(HTTPS)) {
+                        ignoreSsl();
+                        conn = (HttpsURLConnection) url.openConnection();
+                    } else {
+                        conn = (HttpURLConnection) url.openConnection();
+                    }
+                } else {
+                    if (urlString.startsWith(HTTPS)) {
+                        ignoreSsl();
+                        conn = (HttpsURLConnection) url.openConnection(proxy);
+                    } else {
+                        conn = (HttpURLConnection) url.openConnection(proxy);
+                    }
+                }
+                conn.setConnectTimeout(5 * 1000);
+                conn.setDoOutput(true);
+                conn.setRequestMethod(GET);
+                if (heads != null) {
+                    for (String key : heads.keySet()) {
+                        conn.addRequestProperty(key, heads.get(key));
+                    }
+                }
+                is = conn.getInputStream();   //获取输入流，此时才真正建立链接
+                baos = new ByteArrayOutputStream();
+                byte[] temp = new byte[1024];
+                int len;
+                while ((len = is.read(temp)) != -1) {
+                    baos.write(temp, 0, len);
+                }
+                if (iWebCallback != null) {
+                    iWebCallback.onCallback(conn.getResponseCode(), conn.getResponseMessage(), conn.getHeaderFields(), baos.toByteArray());
+                }
+            } catch (Exception e) {
+                int code = 600;
+                try {
+                    code = conn == null ? 600 : conn.getResponseCode();
+                } catch (Exception e1) {
+                }
+                if (iWebCallback != null) {
+                    iWebCallback.onFail(code, e.toString());
+                }
+            } finally {
+                closeSilently(is);
+                closeSilently(baos);
+                if (conn != null) {
+                    conn.disconnect();
+                }
+            }
+        }
+    }
+
+    public void ignoreSsl() {
+        HostnameVerifier hv = new HostnameVerifier() {
+            public boolean verify(String urlHostName, SSLSession session) {
+                return true;
+            }
+        };
+        trustAllHttpsCertificates();
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+
+    public void trustAllHttpsCertificates() {
+        TrustManager[] trustAllCerts = new TrustManager[1];
+        TrustManager tm = new miTM();
+        trustAllCerts[0] = tm;
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+        }
     }
 
     public byte[] getURLResponse(String urlString, HashMap<String, String> headers) {
@@ -200,6 +290,7 @@ public class HttpUtils {
         HttpURLConnection conn;
         URL url = new URL(urlString); //URL对象
         if (urlString.startsWith(HTTPS)) {
+            ignoreSsl();
             conn = (HttpsURLConnection) url.openConnection();
         } else {
             conn = (HttpURLConnection) url.openConnection();
@@ -226,4 +317,29 @@ public class HttpUtils {
             }
         }
     }
+
+    public static class miTM implements TrustManager, X509TrustManager {
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType)
+                throws CertificateException {
+            return;
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType)
+                throws CertificateException {
+            return;
+        }
+    }
+
 }
